92dcfba77b525f150587a5d37ca13a9bf9cb28fe
Avi Sabadosh <asabadosh895@gmail.com>
2025-10-19 13:44:20 -0700

    update blogs with specific details

--- src/blog.js
+++ src/blog.js
@@ -1,32 +1,40 @@
-var Blogs = [
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Blogs = [
     {
-        title: "blog1",
+        title: "The sport of baseball",
         date: "10/15/25",
-        description: "this is my description",
-        image: "apple.jpeg",
-        imageAlt: "desciption of image",
-        slug: "slug"
+        description: "Learn about the history of baseball!",
+        image: "baseball.jpeg",
+        imageAlt: "image of baseball",
+        slug: "baseball.html"
     },
     {
-        title: "blog2",
+        title: "The sport of surfing",
         date: "10/15/25",
-        description: "this is my description",
-        image: "me.jpg",
-        imageAlt: "desciption of image2",
-        slug: "slug2"
+        description: "Learn about the history of surfing",
+        image: "surfing.jpg",
+        imageAlt: "person riding wave on surfboard",
+        slug: "surfing.html"
     },
 ];
-var blogContainer = document.getElementById('blog-container');
-Blogs.forEach(function (blog) {
-    var image = document.createElement("img");
+const blogContainer = document.getElementById('blog-container');
+Blogs.forEach((blog) => {
+    const image = document.createElement("img");
     image.src = blog.image;
     image.alt = blog.imageAlt;
     // Logic for creating and appending blog elements...
-    var newDiv = document.createElement("div");
-    var title = document.createElement("h1");
-    var description = document.createElement("p");
+    const newDiv = document.createElement("div");
+    const title = document.createElement("h1");
+    title.textContent = blog.title;
+    const description = document.createElement("p");
+    description.textContent = blog.description;
+    const date = document.createElement("p");
+    date.textContent = blog.date;
     newDiv.append(title);
+    newDiv.append(date);
     newDiv.append(description);
     newDiv.append(image);
     blogContainer.append(newDiv);
 });
+//# sourceMappingURL=blog.js.map
--- src/blog.ts
+++ src/blog.ts
@@ -9,24 +9,24 @@ type Blog = {
 
 const Blogs: Blog[] = [
     {
-        title: "blog1",
+        title: "The sport of baseball",
         date: "10/15/25",
-        description: "this is my description",
-        image: "apple.jpeg",
-        imageAlt: "desciption of image",
-        slug: "slug"
+        description: "Learn about the history of baseball!",
+        image: "baseball.jpeg",
+        imageAlt: "image of baseball",
+        slug: "baseball.html"
     },
     {
-        title: "blog2",
+        title: "The sport of surfing",
         date: "10/15/25",
-        description: "this is my description",
-        image: "me.jpg",
-        imageAlt: "desciption of image2",
-        slug: "slug2"
+        description: "Learn about the history of surfing",
+        image: "surfing.jpeg",
+        imageAlt: "person riding wave on surfboard",
+        slug: "surfing.html"
     },
 ]
 
-const blogContainer = document.getElementById('blog-container');
+const blogContainer = document.getElementById('blog-container')!;
 Blogs.forEach((blog) => {
 	const image = document.createElement("img");
   image.src = blog.image;
@@ -34,8 +34,13 @@ Blogs.forEach((blog) => {
 	// Logic for creating and appending blog elements...
     const newDiv = document.createElement("div");
     const title = document.createElement("h1");
+    title.textContent = blog.title
     const description = document.createElement("p");
+    description.textContent = blog.description
+    const date = document.createElement("p");
+    date.textContent = blog.date
     newDiv.append(title)
+    newDiv.append(date)
     newDiv.append(description)
     newDiv.append(image)
     blogContainer.append(newDiv)
