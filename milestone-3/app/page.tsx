4c35f16471d20bba46e58cdfbbabdfd4cb65a464
Avi Sabadosh <asabadosh895@gmail.com>
2025-11-26 13:16:36 -0800

    fix: grammar fix

--- milestone-3/app/page.tsx
+++ milestone-3/app/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
           <img src="/me.jpg" alt="Image of me"/>
         </div>
         <div className="about-text">
-         <p> <strong>Who am I: </strong> My name is Avi Sabadosh. I am from Oakland CA and was born in San Fransisco. </p> 
+         <p> <strong>Who am I: </strong> My name is Avi Sabadosh. I am from Oakland, CA and was born in San Francisco. </p> 
          <p> <strong>My hobbies include: </strong> surfing, climbing, snowboarding, baseball, and golf</p>
          </div>
       </div>
