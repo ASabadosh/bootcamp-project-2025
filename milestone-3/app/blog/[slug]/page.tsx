94d9cd5a6188367da0cf7f3085892d3bb6feef9d
Avi Sabadosh <asabadosh895@gmail.com>
2025-11-26 13:17:37 -0800

    feat: created [slug]/page.tsx page to render dynamic blog pages

---
+++ milestone-3/app/blog/[slug]/page.tsx
@@ -0,0 +1,42 @@
+type Props = {
+    params: Promise<{ slug: string }>
+}
+
+async function getBlog(slug: string) {
+	try {
+		// This fetches the blog from an api endpoint that would GET the blog
+		const res = await fetch(`http://localhost:3000/api/Blogs/${slug}`, {
+			cache: "no-store",	
+		})
+		// This checks that the GET request was successful
+		if (!res.ok) {
+			throw new Error("Failed to fetch blog");
+		}
+
+		return res.json();
+	} catch (err: unknown) {
+		console.log(`error: ${err}`);
+		return null;
+		// `` are a special way of allowing JS inside a string
+		// Instead of "error: " + err, we can just do the above
+		// it is simular to formated strings in python --> f"{err}"
+	}
+}
+
+export default async function Blog({ params }: Props) {
+	const { slug } = await params;
+	const blog = await getBlog(slug);
+	if (blog == null){
+		return(
+			<main>
+			<p>Blog is unable to be displayed </p>
+			</main>
+		);
+	}
+	return(
+        <main>
+			<h1>{blog.content_title}</h1>
+			<p className = "blog_paragraph">{blog.content}</p>
+        </main>
+    );
+}
