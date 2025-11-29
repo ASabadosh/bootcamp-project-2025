26c2de461a25fd7a3326f49d5c5d9fd9e5a815c7
Avi Sabadosh <asabadosh895@gmail.com>
2025-11-29 11:38:17 -0800

    fix: imported and used function for parsing date object to string

--- milestone-3/components/blogPreview.tsx
+++ milestone-3/components/blogPreview.tsx
@@ -3,15 +3,17 @@ import style from './blogPreview.module.css'
 import type { Blog } from "../src/database/blogSchema";
 import Image from "next/image";
 import Link from "next/link";
+import { parseCommentTime } from "./commentPreview"
 
 export default function BlogPreview(props: Blog) {
+  const date_string = parseCommentTime(props.date)
   return (
 		// replace everything between the <div> & </div> tags
 		// with your code from earlier milestones
     <div className={style.div}>
       <h3> {props.title} </h3>
       <div>
-        <p>Convert date field to string</p>
+        <p>{date_string}</p>
 		// we will replace img with Image, made to optimize images produced with Next.js
 		<Image src={props.image} alt={props.image_alt} width={500} height={500}/>
         <Link href={`/blog/${props.slug}`}>{props.description}</Link>
