2fd14c4082a2d09f5a7e7b094b6bd0c2ba3ab2a9
Avi Sabadosh <asabadosh895@gmail.com>
2025-12-11 23:09:47 -0800

    feat: created contact form using emailjs

--- milestone-3/components/contactForm.tsx
+++ milestone-3/components/contactForm.tsx
@@ -1,14 +1,32 @@
 "use client";
+
+import { FormEvent } from "react";
+import emailjs from "@emailjs/browser";
 import styles from "./contactForm.module.css";
 
+function ContactForm() {
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
 
-function ContactForm(){
+    try {
+      await emailjs.sendForm(
+        "service_6nvcpao",
+        "template_xqut50r",      
+        e.currentTarget,     
+        "ied3sUdgXt59p3Xh3"
+      );
+    alert("Message sent!");
+    e.currentTarget.reset();
+    } catch (err) {
+      alert("Failed to send message. Please try again.");
+    }
+  }
     return(
     <div>
 <h1 className="contact"> 
           Contact
         </h1>
-        <form id="contact-form">
+        <form className={styles.contact_form} onSubmit={handleSubmit}>
 
         
         <div className={styles.input}>
@@ -16,6 +34,7 @@ function ContactForm(){
         <input className = {styles.area} type="text" id="name" name="name"/>
         </div>
        
+       <input type="hidden" name="time" value={new Date().toString()} />
         
         <div className={styles.input}>
         <label className = {styles.label} htmlFor="email">Email</label>
@@ -24,12 +43,11 @@ function ContactForm(){
 
         <div className={styles.input}>
         <label className = {styles.label} htmlFor="message">Message</label>
-        <textarea className = {styles.area} id="message" name="message">
-        </textarea>
+        <textarea className = {styles.area} id="message" name="message"/>
         </div>
 
         <div className={styles.input}>
-        <input className = {styles.label} type="submit"/>
+        <button className = {styles.label} type="submit">Submit</button>
         </div>
 
         </form>
