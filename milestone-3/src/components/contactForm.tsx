faafe95011d8aec620fda5851e633cdf224f3029
Avi Sabadosh <asabadosh895@gmail.com>
2026-06-15 15:28:56 -0700

    fix: deleted css page

--- milestone-3/src/components/contactForm.tsx
+++ milestone-3/src/components/contactForm.tsx
@@ -2,7 +2,6 @@
 
 import { FormEvent } from "react";
 import emailjs from "@emailjs/browser";
-import styles from "./contactForm.module.css";
 
 function ContactForm() {
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
@@ -28,7 +27,7 @@ function ContactForm() {
           Let's Talk
         </h2>
         </div>
-        <div className="flex flex-row gap-20">
+        <div className="flex flex-row gap-80">
         <form className="flex flex-col gap-3 w-full" onSubmit={handleSubmit}>
 
         <input type="text" id="name" name="name" placeholder="Name"className="bg-[#222] border border-white/10 rounded-lg px-4 py-2.5 text-[14px] text-white placeholder:text-white/30 outline-none focus:border-white/30 w-full"/>
@@ -39,9 +38,7 @@ function ContactForm() {
 
         <textarea id="message" name="message" rows={4} placeholder="Message" className="bg-[#222] border border-white/10 rounded-lg px-4 py-2.5 text-[14px] text-white placeholder:text-white/30 outline-none focus:border-white/30 w-full"/>
 
-        <div className={styles.input}>
-        <button type="submit" className="w-full bg-white text-black text-[13px] font-medium py-3 rounded-lg tracking-wide hover:bg-gray-100 transition-colors duration-300 cursor-pointer mt-1">Send Message</button>
-        </div>  
+        <button type="submit" className="w-full bg-white text-black text-[13px] font-medium py-3 rounded-lg tracking-wide hover:bg-gray-100 transition-colors duration-300 cursor-pointer mt-1">Send Message</button> 
 
         </form>
         <div className="flex flex-col gap-2 w-[500px]">
@@ -49,7 +46,7 @@ function ContactForm() {
         <p className="mb-1">avisabadosh895@gmail.com</p>
         <p className="text-[13px] font-medium text-white/45 tracking-[0.08em] uppercase">Github</p>
         <a href="https://github.com/ASabadosh" className="text-[#85B7EB] hover:underline mb-3" target="_blank" rel="noopener noreferrer">github.com/ASabadosh</a>
-        <button className= "bg-[#2a2a2a] border border-white/[0.12] rounded-lg px-4 py-2.5 text-[13px] font-bold text-white hover:border-white/30 transition-colors cursor-pointer">Download Resume</button>
+        <a href="/Avi_Sabadosh_Resume.pdf" download className= "bg-[#2a2a2a] border border-white/[0.12] rounded-lg px-4 py-2.5 text-[13px] font-bold text-white hover:border-white/30 transition-colors cursor-pointer">Download Resume</a>
         </div>
         </div>
     </div>
