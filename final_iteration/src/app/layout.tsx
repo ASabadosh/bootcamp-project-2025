af7c7f0f2c448594277fd452addf10075da6d87e
Avi Sabadosh <asabadosh895@gmail.com>
2026-06-19 16:24:12 -0700

    fix: bakcticks

--- final_iteration/src/app/layout.tsx
+++ final_iteration/src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={'${geistSans.variable} ${geistMono.variable}'}>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <Navbar/>
         {children}
       </body>
